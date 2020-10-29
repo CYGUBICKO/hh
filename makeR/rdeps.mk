18d8dc731fb90ed29208aa5c24436537a76b4f16
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-10-29 14:16:51 -0400

    add overlooked sources

---
+++ makeR/rdeps.mk
@@ -0,0 +1,71 @@
+## REMOVE rules from here when you implement the makeR
+analysisData.Rout: missing_after_cleaning.rda cleaning.rda globalFunctions.rda
+analysis_variables.Rout: analysisData.rda
+cleaning_plots.Rout: mergeWash.rda
+cleaning.Rout: globalFunctions.rda missingCategory_summary.rda generateLabels.rda
+cleaning_tables.Rout: cleaning.rda
+compareCalculations.Rout: mergeWash.rda
+descriptive_stats.Rout: simplePlotsRuncs.rda analysisData.rda
+dwelling_mca_plot.Rout: analysisData.rda dwelling_mca.rda
+dwelling_mca.Rout: cleanData.rda
+dwelling_pca_plot.Rout: dwelling_pca.rda cleanData.rda
+dwelling_pca.Rout: cleanData.rda
+expenditure_index_plot.Rout: expenditure_index.rda
+expenditure_index.Rout: cleanData.rda
+expenditure_pca.Rout: analysisData.rda
+garbage_condeffect_plots.Rout: garbage_condeffect.rda
+garbage_condeffect.Rout: garbage_glm.rda
+garbage_glm.Rout: washdataStatusPcats.rda
+garbageP_condeffect_plots.Rout: garbageP_condeffect.rda
+garbageP_condeffect.Rout: garbageP_glm.rda
+garbageP_emmeans.Rout: garbageP_glm.rda
+garbageP_glm.Rout: washdataStatusPcats.rda
+garbageP_JD.Rout: garbageP_glm.rda eplotsFuns.rda ordFuns.rda
+garbage_tidy_plots.Rout: garbage_tidy.rda
+garbage_tidy.Rout: garbage_glm.rda garbageP_glm.rda
+generateLabels.Rout: globalFunctions.rda loadData.rda
+hhsize_plot.Rout: cleanData.rda
+hhWashcompare.Rout: mergeWash.rda
+loadData.Rout: globalFunctions.rda
+materials_tables.Rout: generateLabels.rda
+mergeWash.Rout: cleaning.rda globalFunctions.rda
+missing_after_cleaning.Rout: cleaning.rda overall_missing_tables.rda globalFunctions.rda
+missingCategory_summary.Rout: loadData.rda globalFunctions.rda
+overall_missing_tables.Rout: missingCategory_summary.rda cleaning.rda
+ownership_gpca_plot.Rout: ownership_gpca.rda cleanData.rda
+ownership_gpca.Rout: cleanData.rda
+ownership_impute.Rout: analysisData.rda
+ownership_lpca_plot.Rout: cleanData.rda ownership_lpca.rda
+ownership_lpca.Rout: cleanData.rda
+ownership_mca.Rout: ownership_imputeMCA.rda
+ownership_pca_plot.Rout: analysisData.rda ownership_pca.rda
+ownership_pca.Rout: analysisData.rda
+problems_ever_index_plot.Rout: problems_ever_index.rda
+problems_ever_index.Rout: cleanData.rda
+problems_index_plot.Rout: problems_index.rda
+problems_index.Rout: cleanData.rda
+problems_mca.Rout: analysisData.rda
+problems_pca.Rout: analysisData.rda
+resp_predictor_plots.Rout: washData.rda
+shortData.Rout: loadData.rda
+simple_brms_plots.Rout: simple_brms.rda
+simple_brms.Rout: simple_sim.rda
+simple_glm_plots.Rout: simple_brms.rda
+simple_predict_brms.Rout: simple_brms.rda
+washdataInspect_plots.Rout: washdataInspect.rda
+washdataInspect.Rout: washData.rda globalFunctions.rda
+washData.Rout: cleanData.rda problems_index.rda expenditure_index.rda problems_ever_index.rda ownership_gpca.rda dwelling_pca.rda
+washdataStatusPcats.Rout: washdataInspect.rda
+washEffectsize_plots.Rout: washTidyestimates.rda
+washModeldata.Rout: longDFunc.rda washdataStatusPcats.rda
+washModelfit_brms.Rout: washdataStatusPcats.rda
+washModelfit_pglmerS.Rout: washModeldata.rda
+washModelfit_poly_tmbS.Rout: washModeldata.rda
+washModelfit_tmbS.Rout: washModeldata.rda
+washPredEffects_brms_plots.Rout: washPredEffects_brms.rda
+washPredEffects_brms.Rout: washModelfit_brms.rda
+washPredEffects_plots.Rout: washPredEffects.rda
+washPredEffects.Rout: washModelfit_tmbS.rda
+washPredEffects_Resp_plots.Rout: washPredEffects_Resp.rda
+washPredEffects_Resp.Rout: washModelfit_tmbS.rda
+washTidyestimates.Rout: globalFunctions.rda washModelfit_tmbS.rda washModelfit_brms.rda
